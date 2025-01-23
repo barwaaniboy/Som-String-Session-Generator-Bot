998c95d9da3cf5887303698d2bfee3290e0c97d4
barwaaniboy <khaandrama@gmail.com>
2025-01-23 05:12:56 -0800

    Update config.py

--- config.py
+++ config.py
@@ -5,13 +5,13 @@ API_ID = int(environ.get("API_ID", "17461958"))
 API_HASH = environ.get("API_HASH", "0f083cb4779252f82b99f5c644274624")
 
 # Your Main Bot Token 
-BOT_TOKEN = environ.get("BOT_TOKEN", "7826334394:AAEoq0uOycdMJ4-R41icJIfq0tLXRgnYNJw")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 # Owner ID For Broadcasting 
 OWNER_ID = int(environ.get("OWNER_ID", "2081516065")) # Owner Id or Admin Id
 
 # Give Your Force Subscribe Channel Id Below And Make Bot Admin With Full Right.
-F_SUB = environ.get("F_SUB", "-1002308375163")
+F_SUB = environ.get("F_SUB", "")
 
 # Mongodb Database Uri For User Data Store 
 MONGO_DB_URI = environ.get("MONGO_DB_URI", "mongodb+srv://khaanbarwaani:qL47JyUNrzSRp2Aj@cluster0.qgucx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
